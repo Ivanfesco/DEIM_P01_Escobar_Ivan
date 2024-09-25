6568b32743ff32ffd1dc8445486768d20a6e501c
Ivan Escobar <ivanfesco8@gmail.com>
2024-09-25 12:40:01 +0200

    Movement improvement, no longer sliding. Jumping only while grounded

--- DEIM_P01_EscobarIvan/DEIM_P01_Escobar_Ivan/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ DEIM_P01_EscobarIvan/DEIM_P01_Escobar_Ivan/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -9,31 +10,59 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] private Rigidbody2D rigidbod;
-    [SerializeField] private float speed=1;
+    [SerializeField] private float speed = 20;
     [SerializeField] private float jumpspeed = 300;
+    [SerializeField] private Collider2D groundcol;
+    [SerializeField] private TilemapCollider2D tilecol;
+    private bool grounded = false;
+    private float yvelocity = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKey(KeyCode.D)) 
+        yvelocity = rigidbod.velocity.y;
+        if (Input.GetKey(KeyCode.D))
         {
-            rigidbod.AddForce(Vector2.right*speed);
+            rigidbod.AddForce(Vector2.right * speed);
 
         }
-        
+        else
+        {
+            rigidbod.velocity = new Vector2(0, yvelocity);
+
+        }
+
         if (Input.GetKey(KeyCode.A))
         {
             rigidbod.AddForce(Vector2.left * speed);
+
         }
+        else
+        {
+            rigidbod.velocity = new Vector2(0, yvelocity);
+
+        }
+        yvelocity = rigidbod.velocity.y;
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rigidbod.AddForce(Vector2.up * jumpspeed);
+            if (grounded == true)
+            {
+                rigidbod.AddForce(Vector2.up * jumpspeed);
+            }
+        }
+
+        if (groundcol.IsTouching(tilecol))
+            {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
         }
     }
 }
